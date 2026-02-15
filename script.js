ef08db456ca69785361811262a3832e399464c15
shilaCode-Hub <shilarana363@gmail.com>
2026-02-15 13:05:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,21 +10,21 @@ let songItems = Array.from(document.getElementsByClassName('songItem'));
 
 
 let songs =[
-    {songName:"SUPERHERO", filePath:"musics/Alex Hagen - Superhero [NCS Release].mp3",coverPath:"coverImage/Superhero_29.webp"},
+    {songName:"SUPERHERO", filePath:"songs/Superhero.mp3",coverPath:"coverImage/Superhero_29.webp"},
 
-    {songName:"Fearless Funk", filePath:"musics/DR MØB, Chris Linton - Fearless Funk [NCS Release].mp3", coverPath:"coverImage/1200x630bf-60.jpg"},
+    {songName:"Fearless Funk", filePath:"songs/FearlessFunk.mp3", coverPath:"coverImage/1200x630bf-60.jpg"},
 
-    {songName:"FAVELA", filePath:"musics/MXZI, Deno - FAVELA [NCS Release].mp3", coverPath:"coverImage/unnamed.jpg"},
+    {songName:"FAVELA", filePath:"songs/FAVELA.mp3", coverPath:"coverImage/unnamed.jpg"},
 
-    {songName:"STUCKINMYHEAD!", filePath:"musics/youth® - stuckinmyhead! [NCS Release].mp3", coverPath:"coverImage/maxresdefault.jpg"},
+    {songName:"STUCKINMYHEAD!", filePath:"songs/stuckinmyhead.mp3", coverPath:"coverImage/maxresdefault.jpg"},
 
-    {songName:"HARINEZUMI", filePath:"musics/waera - harinezumi [NCS Release].mp3", coverPath:"coverImage/hqdefault.jpg"},
+    {songName:"HARINEZUMI", filePath:"songs/harinezumi.mp3", coverPath:"coverImage/hqdefault.jpg"},
 
-    {songName:"VANDALI", filePath:"musics/Aditya Sharma - VANDALI [NCS Release].mp3", coverPath:"coverImage/unnamed (1).jpg"},
+    {songName:"VANDALI", filePath:"songs/VANDALI.mp3", coverPath:"coverImage/unnamed (1).jpg"},
 
-    {songName:"Where We Are", filePath:"musics/Sync, Triangle, Eytan Peled - Where We Are [NCS Release].mp3", coverPath:"coverImage/ab67616d0000b273aeaadf0538857a477cbf5544.jpg"},
+    {songName:"Where We Are", filePath:"songs/WhereWeAre.mp3", coverPath:"coverImage/ab67616d0000b273aeaadf0538857a477cbf5544.jpg"},
 
-    {songName:"HYPNOTIZED!", filePath:"musics/noaa! - HYPNOTIZED! [NCS Release].mp3", coverPath:"coverImage/size_m.jpg"},
+    {songName:"HYPNOTIZED!", filePath:"songs/HYPNOTIZED.mp3", coverPath:"coverImage/size_m.jpg"},
 
 ]
 songItems.forEach((element,i)=>{
@@ -128,5 +128,4 @@ document.getElementById('previous').addEventListener('click',()=>{
         masterPlay.classList.add('fa-circle-pause');
         gif.style.opacity = 1;
 });
-
 
